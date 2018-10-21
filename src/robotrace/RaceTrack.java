e2f6c5c73b99ba7cb6cfe834dbbc224ea1fd4e88
kenrutten <38913644+kenrutten@users.noreply.github.com>
2018-10-21 10:39:01 +0200

    Created parametric track drawing in RaceTrack.java

--- src/robotrace/RaceTrack.java
+++ src/robotrace/RaceTrack.java
@@ -1,5 +1,6 @@
 package robotrace;
 
+import static com.jogamp.opengl.GL.GL_TEXTURE0;
 import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
@@ -12,9 +13,8 @@ abstract class RaceTrack {
     
     /** The width of one lane. The total width of the track is 4 * laneWidth. */
     private final static float laneWidth = 1.22f;
-    private final static float TotalLaneWidth = 4*laneWidth;
     
-    
+    private final static float interval = 1f/1000;
     
     /**
      * Constructor for the default track.
@@ -28,7 +28,62 @@ abstract class RaceTrack {
      * Draws this track, based on the control points.
      */
     public void draw(GL2 gl, GLU glu, GLUT glut) {
+        Vector normal = getTangent(0).cross(Vector.Z).normalized();
         
+        //DRAW SURFACE
+        Textures.track.bind(gl);
+        gl.glBegin(GL2.GL_TRIANGLE_STRIP);
+         gl.glNormal3d(normal.x(), normal.y(), normal.z());
+        
+        for(float t = 0; t < 1; t += interval){
+           Vector center = getPoint(t);
+           normal = getTangent(t).cross(Vector.Z).normalized();
+           
+           Vector in = center.subtract(normal.scale(2*laneWidth));
+           Vector out = center.add(normal.scale(2*laneWidth));
+           
+            gl.glMultiTexCoord2d(GL_TEXTURE0, 0, 0);
+                gl.glVertex3d(in.x, in.y,  1);
+                gl.glMultiTexCoord2d(GL_TEXTURE0, 1, 0);
+                gl.glVertex3d(out.x, out.y, 1);
+        }
+        gl.glEnd();
+        
+        //DRAW INSIDE WALL
+         Textures.brick.bind(gl);
+        gl.glBegin(GL2.GL_TRIANGLE_STRIP);
+         gl.glNormal3d(normal.x(), normal.y(), normal.z());
+        
+        for(float t = 0; t < 1; t += interval){
+           Vector center = getPoint(t);
+           normal = getTangent(t).cross(Vector.Z).normalized();
+           
+           Vector in = center.subtract(normal.scale(2*laneWidth));
+           
+            gl.glMultiTexCoord2d(GL_TEXTURE0, 0, 0);
+                gl.glVertex3d(in.x, in.y,  1);
+                gl.glMultiTexCoord2d(GL_TEXTURE0, 1, 0);
+                gl.glVertex3d(in.x, in.y,  -1);
+        }
+        gl.glEnd();
+        
+         //DRAW OUTSIDE WALL
+        Textures.brick.bind(gl);
+        gl.glBegin(GL2.GL_TRIANGLE_STRIP);
+         gl.glNormal3d(normal.x(), normal.y(), normal.z());
+        
+        for(float t = 0; t < 1; t += interval){
+           Vector center = getPoint(t);
+           normal = getTangent(t).cross(Vector.Z).normalized();
+           
+           Vector out = center.add(normal.scale(2*laneWidth));
+           
+            gl.glMultiTexCoord2d(GL_TEXTURE0, 0, 0);
+                gl.glVertex3d(out.x, out.y,  1);
+                gl.glMultiTexCoord2d(GL_TEXTURE0, 1, 0);
+                gl.glVertex3d(out.x, out.y, -1);
+        }
+        gl.glEnd();
     }
     
     /**
@@ -52,10 +107,10 @@ abstract class RaceTrack {
     }
     
     
-    
     // Returns a point on the test track at 0 <= t < 1.
     protected abstract Vector getPoint(double t);
-
+    
+    
     // Returns a tangent on the test track at 0 <= t < 1.
     protected abstract Vector getTangent(double t);
 }
