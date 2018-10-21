eef5720203bfc05882c0e983ff988eebbb6efb2e
kenrutten <38913644+kenrutten@users.noreply.github.com>
2018-10-21 09:50:45 +0200

    Improved the robot in Robot.java

--- src/robotrace/Robot.java
+++ src/robotrace/Robot.java
@@ -38,68 +38,94 @@ class Robot {
      * Draws this robot (as a {@code stickfigure} if specified).
      */
     public void draw(GL2 gl, GLU glu, GLUT glut, float tAnim) {
-        gl.glColor3d(255,0,0);
-        gl.glPushMatrix(); 
+        //gl.glColor3d(255,0,0);
+        gl.glPushMatrix();
+        gl.glRotated(90*(-direction.x+direction.y+direction.z), 0, 0, 1);
+        gl.glTranslated(position.x, position.y, position.z);
+        
+         
             //Draw right leg
-            gl.glTranslated(0.12, 0.15,0.05);
-            gl.glScaled(0.2, 0.3, 0.1);
-            glut.glutSolidCube(1); //foot
-            gl.glScaled(5, 3.33, 10);
-            gl.glTranslated(0, -0.07, 0.09);
+        gl.glPushMatrix();
+        
+        
+        gl.glColor3d(255,0,0);
+            gl.glTranslated(0.12, 0.08, 0.88);
+            gl.glRotated(30, 1, 0, 0); //ROtation of the complete leg
+            
+            gl.glTranslated(0, 0, -0.17);
+            gl.glScaled(0.14, 0.14, 0.34);
+            glut.glutSolidCube(1); //Upper leg
+            gl.glScaled(7.143, 7.143, 2.941);
+            
+            gl.glColor3d(0,0,255);
+            gl.glTranslated(0, 0.01, -0.19);
+            glut.glutSolidSphere(0.1, 20, 20);//knee
+            
+            gl.glRotated(-30, 1, 0, 0);
+            
+            gl.glColor3d(255,0,0);
+            gl.glTranslated(0, -0.01, -0.19);
+            gl.glScaled(0.12, 0.12, 0.34);
+            glut.glutSolidCube(1);//Lower leg
+             gl.glScaled(8.333, 8.333, 2.941);
+            
+             gl.glTranslated(0, 0, -0.19);
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.08, 20, 20);//ancle
-            gl.glTranslated(0,0,0.19);
-            gl.glScaled(0.12, 0.12, 0.34);
-            gl.glColor3d(255,0,0);
-            glut.glutSolidCube(1);//lower leg
-            gl.glScaled(8.333, 8.333, 2.941);
-            gl.glTranslated(0, 0.01, 0.19);
-            gl.glColor3d(0,0,255);
-            glut.glutSolidSphere(0.1, 20, 20);//knee
-            gl.glTranslated(0, -0.01, 0.19);
-            gl.glScaled(0.14, 0.14, 0.34);
-            gl.glColor3d(255,0,0);
-            glut.glutSolidCube(1);//upper leg
-            gl.glScaled(7.143, 7.143, 2.941);
             
-            //Draw left leg
-            gl.glTranslated(-0.24, 0.08, -0.66);
+            gl.glColor3d(255,0,0);
+            gl.glTranslated(0, 0.07, -0.09);
             gl.glScaled(0.2, 0.3, 0.1);
-            glut.glutSolidCube(1);
-            gl.glScaled(5, 3.33, 10);
-            gl.glTranslated(0, -0.07, 0.09);
-            gl.glColor3d(0,0,255);
-            glut.glutSolidSphere(0.08, 20, 20);
-            gl.glTranslated(0,0,0.19);
-            gl.glScaled(0.12, 0.12, 0.34);
-            gl.glColor3d(255,0,0);
-            glut.glutSolidCube(1);
-            gl.glScaled(8.333, 8.333, 2.941);
-            gl.glTranslated(0, 0.01, 0.19);
-            gl.glColor3d(0,0,255);
-            glut.glutSolidSphere(0.1, 20, 20);
-            gl.glTranslated(0, -0.01, 0.19);
+            glut.glutSolidCube(1); //foot
+        gl.glPopMatrix();
+        
+          //Draw right leg
+        gl.glPushMatrix();
+        gl.glColor3d(255,0,0);
+            gl.glTranslated(-0.12, 0.08,0.71);
             gl.glScaled(0.14, 0.14, 0.34);
-            gl.glColor3d(255,0,0);
-            glut.glutSolidCube(1);
+            glut.glutSolidCube(1); //Upper leg
             gl.glScaled(7.143, 7.143, 2.941);
             
+            gl.glColor3d(0,0,255);
+            gl.glTranslated(0, 0.01, -0.19);
+            glut.glutSolidSphere(0.1, 20, 20);//knee
+            
+            gl.glColor3d(255,0,0);
+            gl.glTranslated(0, -0.01, -0.19);
+            gl.glScaled(0.12, 0.12, 0.34);
+            glut.glutSolidCube(1);//Lower leg
+             gl.glScaled(8.333, 8.333, 2.941);
+            
+             gl.glTranslated(0, 0, -0.19);
+            gl.glColor3d(0,0,255);
+            glut.glutSolidSphere(0.08, 20, 20);//ancle
+            
+            gl.glColor3d(255,0,0);
+            gl.glTranslated(0, 0.07, -0.09);
+            gl.glScaled(0.2, 0.3, 0.1);
+            glut.glutSolidCube(1); //foot
+        gl.glPopMatrix();
+        
             //draw torso
-            gl.glTranslated(0.12, 0, 0.4);
+        gl.glPushMatrix();
+            gl.glTranslated(0, 0.08, 1.11);
             gl.glScaled(0.4, 0.2, 0.5);
             glut.glutSolidCube(1);//Belly
             gl.glScaled(2.5, 5, 2);
             gl.glTranslated(0, 0, 0.41);
             gl.glScaled(0.6, 0.26, 0.32);
             glut.glutSolidCube(1);//Shoulders
-            gl.glScaled(1.667, 3.846, 3.125);
+        gl.glPopMatrix();
+        
             
-            //draw left arm
-            gl.glTranslated(0.32, 0, 0);
+          //draw left arm
+          gl.glPushMatrix();
+            gl.glTranslated(0.32, 0.08, 1.52);
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.1, 20, 20);
-            //gl.glRotated(70, 0, 1, 0);
-            //gl.glRotated(40, 0, 0, 1);
+            gl.glRotated(70, 0, 1, 0);
+            gl.glRotated(40, 0, 0, 1);
             gl.glTranslated(0.2, 0, 0);
             gl.glScaled(0.28, 0.12, 0.12);
             gl.glColor3d(255,0,0);
@@ -108,7 +134,7 @@ class Robot {
             gl.glTranslated(0.15, 0, 0);
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.08, 20, 20);//elbow
-           // gl.glRotated(40, 0, 0, 1);
+            gl.glRotated(40, 0, 0, 1);
             gl.glTranslated(0.16, 0, 0);
             gl.glScaled(0.3, 0.1, 0.1);
             gl.glColor3d(255,0,0);
@@ -118,13 +144,19 @@ class Robot {
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.06, 20, 20);
             gl.glTranslated(-0.94, 0, 0);
+            gl.glRotated(-80, 0, 0, 1);
+            gl.glRotated(-70, 0, 1, 0);
+        gl.glPopMatrix();
+        
+        
             
             //draw right arm
-              gl.glTranslated(-0.32, 0, 0);
+        gl.glPushMatrix();
+            gl.glTranslated(-0.32, 0.08, 1.52);
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.1, 20, 20);
-            //gl.glRotated(70, 0, 1, 0);
-            //gl.glRotated(40, 0, 0, 1);
+            gl.glRotated(-70, 0, 1, 0);
+            gl.glRotated(-40, 0, 0, 1);
             gl.glTranslated(-0.2, 0, 0);
             gl.glScaled(0.28, 0.12, 0.12);
             gl.glColor3d(255,0,0);
@@ -133,7 +165,7 @@ class Robot {
             gl.glTranslated(-0.15, 0, 0);
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.08, 20, 20);//elbow
-            //gl.glRotated(40, 0, 0, 1);
+            gl.glRotated(-40, 0, 0, 1);
             gl.glTranslated(-0.16, 0, 0);
             gl.glScaled(0.3, 0.1, 0.1);
             gl.glColor3d(255,0,0);
@@ -143,12 +175,16 @@ class Robot {
             gl.glColor3d(0,0,255);
             glut.glutSolidSphere(0.06, 20, 20);
             gl.glTranslated(0.94, 0, 0);
-            
+        gl.glPopMatrix();
+        
             //draw head
-            gl.glTranslated(0, 0, 0.26);
+        gl.glPushMatrix();
+            gl.glTranslated(0, 0.08, 1.78);
             gl.glScaled(0.2, 0.2, 0.2);
             glut.glutSolidCube(1);
         gl.glPopMatrix();
+        
+      gl.glPopMatrix();
     }
     
     
