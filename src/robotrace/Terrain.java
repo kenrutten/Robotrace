d1589dd55d267c2914630d2d0a831eb88dd4082f
s164689 <mohamedyehia1998@gmail.com>
2018-10-23 12:11:08 +0200

    Bz track freezes for some reason

--- src/robotrace/Terrain.java
+++ src/robotrace/Terrain.java
@@ -4,15 +4,20 @@ import com.jogamp.opengl.util.gl2.GLUT;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.glu.GLU;
 
+
+import com.jogamp.opengl.util.gl2.GLUT;
+import com.jogamp.opengl.GL2;
+import com.jogamp.opengl.glu.GLU;
+
 /**
  * Represents the terrain, to be implemented according to the Assignments.
  */
 class Terrain {
 
-    
-    
+
+
     public Terrain() {
-        
+
     }
 
     /**
@@ -21,5 +26,5 @@ class Terrain {
     public void draw(GL2 gl, GLU glu, GLUT glut) {
 
     }
-    
+
 }
