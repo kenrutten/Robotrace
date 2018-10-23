29b6342e721af9c19dfd5ee33c1414e457b2604b
kenrutten <38913644+kenrutten@users.noreply.github.com>
2018-10-23 18:48:25 +0200

    Update vertex.glsl

--- src/robotrace/shaderPrograms/Track/vertex.glsl
+++ src/robotrace/shaderPrograms/Track/vertex.glsl
@@ -1,7 +1,21 @@
-// simple vertex shader
+#version 120
 
-void main()
-{
-    gl_Position    = gl_ModelViewProjectionMatrix * gl_Vertex;      // model view transform
-    gl_FrontColor = gl_Color;
+uniform bool ambient, diffuse, specular;
+varying vec3 N;
+varying vec3 P;
+
+
+void main() {
+	// pick up light LIGHT0 and material properties set in ShaderMaker
+        gl_LightSourceParameters light = gl_LightSource[0];
+	gl_MaterialParameters mat = gl_FrontMaterial;
+
+	
+	N = normalize(gl_NormalMatrix*gl_Normal); 
+	P = (gl_ModelViewMatrix*gl_Vertex).xyz; 
+
+	// output of vertex shader
+	gl_TexCoord[0] = gl_MultiTexCoord0;
+	//gl_FrontColor = shading(P, N, light, mat);
+	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 }
